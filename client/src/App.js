32de0074278dd4efad27fec23bcb864cc24b0cc8
ilMatadore <restarter.dev@gmail.com>
2020-04-04 19:51:37 -0500

    styling mobile

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import CheckoutPage from './components/checkout/checkout.component'
+import CheckoutPage from './components/checkout/checkout.component';
+
+import { GlobalStyle} from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selector';
 import { checkUserSession } from './redux/user/user.actions';
@@ -23,6 +23,7 @@ const App = ( {checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
